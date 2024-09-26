e07ea5079a84cc84f5075b153b1fbb4b9856b496
Alex Christy <a.christy@ufl.edu>
2024-09-25 21:41:02 -0400

    Working directory import

--- import_ova.sh
+++ import_ova.sh
@@ -2,7 +2,7 @@
 
 # Globals
 START_VMID=0
-REQUIRED_COMMANDS="tar qm lvremove gunzip qemu-img"
+REQUIRED_COMMANDS="tar qm lvremove gunzip qemu-img expr"
 
 # Function to display help message
 show_help() {
@@ -10,9 +10,8 @@ show_help() {
     echo
     echo "Options:"
     echo "  -h, --help          Show this help message and exit"
-    echo "  -f, --file          Specify a .ova file to process"
-    echo "  -d, --directory     Specify a directory of .ova files to process"
-    echo "  -v, --verbose       Enable verbose mode"
+    echo "  -f, --file          Specify a single .ova file to process"
+    echo "  -d, --directory     Specify a directory of .ova files to process recursively"
     echo
 }
 
@@ -29,92 +28,130 @@ check_commands() {
 }
 
 import_ova_file() {
-        _import_ova_file_path=$1
-        _import_ova_file_vmid=$2
-        _import_ova_file_dir=$(dirname "$_import_ova_file_path")
+    _import_ova_file_path=$1
+    _import_ova_file_vmid=$2
+    _import_ova_file_dir=$(dirname "$_import_ova_file_path")
 
-        # Check that the file path exists
-        if [ ! -f "$_import_ova_file_path" ]; then
-                echo "Could not find file: $_import_ova_file_path"
-                return 1
-        fi
+    # Check that the file path exists
+    if [ ! -f "$_import_ova_file_path" ]; then
+            echo "Could not find file: $_import_ova_file_path"
+            return 1
+    fi
 
-        # Check file extension is .ova
-        if [ ! "${_import_ova_file_path##*.}" = "ova" ]; then
-                echo "File is not an OVA file: $_import_ova_file_path"
-                return 1
-        fi
+    # Check file extension is .ova
+    if [ ! "${_import_ova_file_path##*.}" = "ova" ]; then
+            echo "File is not an OVA file: $_import_ova_file_path"
+            return 1
+    fi
 
-        # Extract filename without extension
-        _import_ova_file_filename="${_import_ova_file_path##*/}"
-        _import_ova_file_filename_no_extension="${_import_ova_file_filename%.*}"
-	
-	# Extract .ova file
-        echo "Found OVA file: $_import_ova_file_filename"
-        echo "Extracting OVA file..."
-	cd "$_import_ova_file_dir" || echo "Could not enter directory: $_import_ova_file_dir."
-	tar -xvf "./$_import_ova_file_path" > /dev/null
-	
+    # Extract filename without extension
+    _import_ova_file_filename="${_import_ova_file_path##*/}"
+    _import_ova_file_filename_no_extension="${_import_ova_file_filename%.*}"
 
-        # Import OVF
-        _file_extensions=".ovf .ova.ovf"
-        _import_ova_file_found_ovf=0
-        for _file_extension in $_file_extensions; do
-                if [ -f "${_import_ova_file_filename_no_extension}${_file_extension}" ]; then
-                        echo "Found OVF file: ${_import_ova_file_filename_no_extension}${_file_extension}"
-                        echo "Importing OVF file..."
-                        qm importovf "$_import_ova_file_vmid" "${_import_ova_file_filename_no_extension}${_file_extension}" local-lvm > /dev/null
-                        _import_ova_file_found_ovf=1
-                        break
-                fi
-        done
+    # Extract .ova file
+    echo "Found OVA file: $_import_ova_file_filename"
+    echo "Extracting OVA file..."
+    cd "$_import_ova_file_dir" || echo "Could not enter directory: $_import_ova_file_dir."
+    tar -xvf "$_import_ova_file_path" > /dev/null
+    if [ $? -ne 0 ]; then
+        exit_fail
+    fi
 
-        if [ $_import_ova_file_found_ovf -ne 1 ]; then
-                echo "Extracted OVA is missing the OVF: ${_import_ova_file_filename_no_extension}${_file_extension}"
-        fi
 
-        # Delete the imported disk
-        # This is because the imported VMDK doesn't work normally
-        # This also assumes there is only one disk per VM
-        echo "Deleting initial disk..."
-        qm set "$_import_ova_file_vmid" -delete scsi0 > /dev/null
-        qm set "$_import_ova_file_vmid" -delete unused0 > /dev/null
-        lvremove "/dev/pve/vm-$_import_ova_file_vmid-disk-0" > /dev/null 2>&1
+    # Import OVF
+    _file_extensions=".ovf .ova.ovf"
+    _import_ova_file_found_ovf=0
+    for _file_extension in $_file_extensions; do
+            if [ -f "${_import_ova_file_filename_no_extension}${_file_extension}" ]; then
+                    echo "Found OVF file: ${_import_ova_file_filename_no_extension}${_file_extension}"
+                    echo "Importing OVF file..."
+                    qm importovf "$_import_ova_file_vmid" "${_import_ova_file_filename_no_extension}${_file_extension}" local-lvm > /dev/null
+                    _import_ova_file_found_ovf=1
+                    break
+            fi
+    done
 
-        # Unzip the VMDK file if needed
-        _import_ova_file_vmdk_file=""
-        for _file in "$_import_ova_file_filename_no_extension"*disk*.vmdk*; do
-                _import_ova_file_vmdk_file="$_file"
-                
-                # Get the file extension using parameter expansion (faster than awk)
-                _file_extension="${_import_ova_file_vmdk_file##*.}"
-                
-                if [ "$_file_extension" = "gz" ]; then
-                        echo "Unzipping VMDK disk file..."
-                        gzip -d "$_import_ova_file_vmdk_file"
-                        break  # Exit after handling the first gzipped file
-                fi
-        done
+    if [ $_import_ova_file_found_ovf -ne 1 ]; then
+            echo "Extracted OVA is missing the OVF: ${_import_ova_file_filename_no_extension}${_file_extension}"
+            exit_fail
+    fi
 
-        if [ -z "$_import_ova_file_vmdk_file" ]; then
-                echo "Failed to find a VMDK file from the extracted OVA: $_import_ova_file_path"
-                exit_fail
-        fi
+    # Delete the imported disk
+    # This is because the imported VMDK doesn't work normally
+    # This also assumes there is only one disk per VM
+    echo "Deleting initial disk..."
+    qm set "$_import_ova_file_vmid" -delete scsi0 > /dev/null
+    qm set "$_import_ova_file_vmid" -delete unused0 > /dev/null
+    lvremove "/dev/pve/vm-$_import_ova_file_vmid-disk-0" > /dev/null 2>&1
 
-        # Convert the VMDK to a compressed Qcow2 image
-        echo "Converting VMDK disk to Qcow2..."
-        _import_ova_file_qcow_image="$_import_ova_file_filename_no_extension.qcow2"
-        qemu-img convert -f vmdk -O qcow2 -c "$_import_ova_file_filename_no_extension.ova-disk1.vmdk" "$_import_ova_file_qcow_image"
+    # Unzip the VMDK file if needed
+    _import_ova_file_vmdk_file=""
+    for _file in "$_import_ova_file_filename_no_extension"*disk*.vmdk*; do
+            _import_ova_file_vmdk_file="$_file"
+            
+            # Get the file extension using parameter expansion (faster than awk)
+            _file_extension="${_import_ova_file_vmdk_file##*.}"
+            
+            if [ "$_file_extension" = "gz" ]; then
+                    echo "Unzipping VMDK disk file..."
+                    gzip -d "$_import_ova_file_vmdk_file"
+                    break  # Exit after handling the first gzipped file
+            fi
+    done
 
-        # Import converted image
-        echo "Importing Qcow2 image..."
-        qm importdisk "$_import_ova_file_vmid" "$_import_ova_file_qcow_image" local-lvm > /dev/null
-        qm set "$_import_ova_file_vmid" --scsi0 "local-lvm:vm-$_import_ova_file_vmid-disk-0" > /dev/null
+    if [ -z "$_import_ova_file_vmdk_file" ]; then
+            echo "Failed to find a VMDK file from the extracted OVA: $_import_ova_file_path"
+            exit_fail
+    fi
 
-        echo "Cleaning up files..."
-        cleanup_files "$_import_ova_file_dir" "$_import_ova_file_filename_no_extension"
-        echo "Succesfully imported: $_import_ova_file_filename_no_extension (VMID: $_import_ova_file_vmid)"
+    # Convert the VMDK to a compressed Qcow2 image
+    echo "Converting VMDK disk to Qcow2..."
+    _import_ova_file_qcow_image="$_import_ova_file_filename_no_extension.qcow2"
+    qemu-img convert -f vmdk -O qcow2 -c "$_import_ova_file_filename_no_extension.ova-disk1.vmdk" "$_import_ova_file_qcow_image"
+
+    # Import converted image
+    echo "Importing Qcow2 image..."
+    qm importdisk "$_import_ova_file_vmid" "$_import_ova_file_qcow_image" local-lvm > /dev/null
+    qm set "$_import_ova_file_vmid" --scsi0 "local-lvm:vm-$_import_ova_file_vmid-disk-0" > /dev/null
+
+    echo "Cleaning up files..."
+    cleanup_files "$_import_ova_file_dir" "$_import_ova_file_filename_no_extension"
+    echo "Succesfully imported: $_import_ova_file_filename_no_extension (VMID: $_import_ova_file_vmid)"
 }       
+
+# Function to convert relative path to absolute path
+get_absolute_path() {
+    _relative_path=$1
+
+    # If the path starts with "/", it is already an absolute path
+    case "$_relative_path" in
+        /*) 
+            echo "$_relative_path"
+            ;;
+        *)
+            # Convert the relative path to an absolute path using pwd
+            echo "$(cd "$(dirname "$_relative_path")" && pwd)/$(basename "$_relative_path")"
+            ;;
+    esac
+}
+
+import_directory() {
+    # Convert directory to an absolute path
+    _import_directory_dir=$(get_absolute_path "$1")
+    validate_directory_exists "$_import_directory_dir"
+
+    find "$_import_directory_dir" -type f -name '*.ova' | while IFS= read -r _import_directory_ova_file
+    do
+        echo "Found OVA file: $_import_directory_ova_file"
+        
+        # Ensure the OVA file path is absolute as well
+        _absolute_ova_file=$(get_absolute_path "$_import_directory_ova_file")
+        
+        # Call the import function with the absolute path
+        import_ova_file "$_absolute_ova_file" "$START_VMID"
+        START_VMID=$((START_VMID + 1))
+    done
+}
 
 validate_vmid() {
     _validate_vmid_vmid="$1"
@@ -181,12 +218,14 @@ generate_random_vmid() {
 }
 
 find_valid_vmid_range() {
+    _find_valid_vmid_range_size=$1
+
     while :; do
         # Generate a random VMID
         _find_valid_vmid_range_random_vmid=$(generate_random_vmid)
 
-        # Validate the VMID with a range of 20
-        if validate_vmid "$_find_valid_vmid_range_random_vmid" 20; then
+        # Validate the VMID with specified range
+        if validate_vmid "$_find_valid_vmid_range_random_vmid" $_find_valid_vmid_range_size; then
            break 
         fi
     done
@@ -226,9 +265,6 @@ if [ $? -ne 0 ]; then
     exit_fail
 fi
 
-# Find a valid VMID range for imported images
-START_VMID=$(find_valid_vmid_range)
-
 # Parse command-line args
 while [ "$1" != "" ]; do
     case $1 in
@@ -242,13 +278,29 @@ while [ "$1" != "" ]; do
             ;;
         -f | --file )
             shift
+
+            # Find a VMID if one was not specified
+            if [ "$START_VMID" -eq 0 ]; then
+                echo "Looking for an unused VMID..."
+                START_VMID=$(find_valid_vmid_range 1)
+                echo "Using VMID: $START_VMID"
+            fi
+            
             validate_vmid "$START_VMID" 1
-            import_ova_file "$1" "$START_VMID" # When importing only one VM just use the START_VMID directly
+            import_ova_file "$(get_absolute_path "$1")" "$START_VMID" # When importing only one VM just use the START_VMID directly
             exit_success
             ;;
         -d | --directory )
             shift
             num_ova_files=$(count_ova_files_in_directory "$1")
+
+            # Find a VMID if one was not specified
+            if [ "$START_VMID" -eq 0 ]; then
+                echo "Looking for a range $num_ova_files unused VMIDs..."
+                START_VMID=$(find_valid_vmid_range 1)
+                echo "Using start VMID: $START_VMID"
+            fi
+
             validate_vmid "$START_VMID" "$num_ova_files"
             import_directory "$1"
             exit_success
